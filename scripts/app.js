5abd83138816dd5e8edc18df508048d81616672b
Infologs <webboostings@gmail.com>
2017-05-30 14:29:30 +0530

    How to use the routing concept in angularjs

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+﻿// Code goes here
+var app = angular.module("MyApp",["ngRoute"]);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when("/",{
+      template:"<h1>This is the main page</h1>"
+    })
+    .when("/red",{
+      templateUrl: "red.html",
+      controller:"cnt1"
+    })
+    .when("/green",{
+      templateUrl: "green.html",
+      controller:"cnt2"
+    })
+    .otherwise({
+      template:"<h1>no page found</h1>"
+    })
+});
+
+app.controller("cnt1", ["$scope",function($scope){
+
+  $scope.message = "From Red page";
+
+}]);
+
+app.controller("cnt2", ["$scope",function($scope){
+
+  $scope.message = "From Green page";
+
+}]);
